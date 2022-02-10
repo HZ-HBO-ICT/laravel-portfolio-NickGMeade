9b84483e22ad3b81183160826409a463e4a3a77f
NickGMeade <mead0001@hz.nl>
2022-02-10 15:26:17 +0100

    Updated comments for pages in dynamic list of FAQs

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    /**
+     * @param $slug - the wildcard to compare with the DBs data
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View - the view of the post page
+     */
     public function show($slug)
     {
         return view('post', [
