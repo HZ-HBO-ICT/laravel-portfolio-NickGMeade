a48838683aa3e35032858884853869883d2628d8
NickGMeade <mead0001@hz.nl>
2022-02-11 18:45:18 +0100

    Correcting automated feedback for week 1 code

--- app/Models/Grade.php
+++ app/Models/Grade.php
@@ -9,13 +9,16 @@ class Grade extends Model
 {
     use HasFactory;
 
+    /**
+     * Function to accept a grade and add it to the database
+     *
+     * @param $result - the new grade result
+     */
     public function addResult($result)
     {
-        if ($result >= $this->best_grade)
-        {
+        if ($result >= $this->best_grade) {
             $this->best_grade = $result;
-            if ($this->best_grade >= $this->lowest_passing_grade)
-            {
+            if ($this->best_grade >= $this->lowest_passing_grade) {
                 $this->passed_at = now();
             }
             $this->save();
